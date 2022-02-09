3f8e9444e0c835deb9f0bdc62dcede58aceee164
Leon Stańczyk <killleon007@gmail.com>
2022-02-09 18:44:39 +0100

    create Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+ 
+import Navbar from "./components/Navbar.js";
+import Cook from './pages/cook/Cook.js'
+import Stock from './pages/stock/Stock'
+import CreateProduct from './pages/createProduct/CreateProduct'
+import ShoppingList from './pages/shoppingList/ShoppingList'
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-App
+      <BrowserRouter>
+      <Navbar />
+        <Routes>
+          <Route path="/" element={<Cook />}/>
+          <Route path="/stock" element={<Stock />} />
+          <Route path="/createProduct" element={<CreateProduct />} />
+          <Route path="/shoppingList" element={<ShoppingList />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
