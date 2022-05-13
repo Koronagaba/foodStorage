56ae71229bc3423ececa11b7250a86030f9a0d7c
koronagaba <killleon007@gmail.com>
2022-05-13 15:27:01 +0200

    Update firebase config file

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,13 +2,24 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDNU3vZxxB8Np5OJnyh4zkwZ1nxSvFs26s",
-    authDomain: "foodstorage-2f9df.firebaseapp.com",
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_API_AUTH_DOMAIN,
     projectId: "foodstorage-2f9df",
-    storageBucket: "foodstorage-2f9df.appspot.com",
-    messagingSenderId: "216365345472",
-    appId: "1:216365345472:web:843971d3c66acdbac61b25"
+    storageBucket: process.env.REACT_APP_API_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_API_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_API_APP_ID
   };
+
+  
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDNU3vZxxB8Np5OJnyh4zkwZ1nxSvFs26s",
+//   authDomain: "foodstorage-2f9df.firebaseapp.com",
+//   projectId: "foodstorage-2f9df",
+//   storageBucket: "foodstorage-2f9df.appspot.com",
+//   messagingSenderId: "216365345472",
+//   appId: "1:216365345472:web:843971d3c66acdbac61b25"
+// };
+
 
   // init firebase
 const app = initializeApp(firebaseConfig);
@@ -17,3 +28,6 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app)
 
 export { db }
+
+
+
