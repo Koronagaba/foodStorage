40c9cb437bd59afd2f7dffda1299aaee36f20abe
koronagaba <killleon007@gmail.com>
2022-05-20 11:45:08 +0200

    Styling Cook site. - first step with CSS Grid

--- src/pages/cook/Cook.tsx
+++ src/pages/cook/Cook.tsx
@@ -2,8 +2,22 @@ import './Cook.css'
 
 const Cook = () => {
   return (
-  <div className="home">
-      <h3>Let's Cook</h3>
+  <div className="cook-container">
+     <div className="new-recipe">
+       <h1>
+         New Recipe
+       </h1>
+     </div>
+     <div className="recipes">
+       <h1>
+        Recipes
+       </h1>
+     </div>
+     <div className="ingredients">
+       <h1>
+        Cook with single ingredients
+       </h1>
+     </div>
   </div>
   );
 };
