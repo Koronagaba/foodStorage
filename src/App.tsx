257bb7543c5cb0d1d3165a56bf256018c78ae33f
koronagaba <killleon007@gmail.com>
2022-05-20 12:44:40 +0200

    Cook navigate with ReactRouter

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import ShoppingList from "./pages/shoppingList/ShoppingList";
 
 import "./App.css";
 import CreateProduct from "./components/createProduct/CreateProduct";
+import RecipeList from "./pages/cook/RecipeList";
+import Ingredients from "./pages/cook/Ingredients";
+import NewRecipe from "./pages/cook/NewRecipe";
 
 function App() {
   const [toggleModal, setToggleModal] = useState(false)
@@ -17,7 +20,10 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Cook />} />
+          <Route path="/cook" element={<Cook />} />
+          <Route path="/cook/newRecipe" element={<NewRecipe />} />
+          <Route path="/cook/recipes" element={<RecipeList />} />
+          <Route path="/cook/ingredients" element={<Ingredients />} />
           <Route path="/createNewProduct" element={<CreateProduct setToggleModal={setToggleModal}/>}/>
           <Route path="/stock" element={<Stock />}>
             <Route path="/stock/createProduct" element={<Stock />} />
--- src/pages/cook/Cook.tsx
+++ src/pages/cook/Cook.tsx
@@ -1,24 +1,22 @@
-import './Cook.css'
+import { NavLink } from "react-router-dom";
+
+import "./Cook.css";
 
 const Cook = () => {
   return (
-  <div className="cook-container">
-     <div className="new-recipe">
-       <h1>
-         New Recipe
-       </h1>
-     </div>
-     <div className="recipes">
-       <h1>
-        Recipes
-       </h1>
-     </div>
-     <div className="ingredients">
-       <h1>
-        Cook with single ingredients
-       </h1>
-     </div>
-  </div>
+    <div className="cook-container">
+      <NavLink to={"newRecipe"} className="new-recipe">
+        <h1>New recipe</h1>
+      </NavLink>
+
+      <NavLink to={"recipes"} className="recipes">
+        <h1>Recipes</h1>
+      </NavLink>
+      
+      <NavLink to={"ingredients"} className="ingredients">
+        <h1>Cook with single ingredients</h1>
+      </NavLink>
+    </div>
   );
 };
 
