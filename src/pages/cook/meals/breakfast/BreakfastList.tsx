5fa612b31f591427e4f5a429c447f2b8cff3c5b8
koronagaba <killleon007@gmail.com>
2022-05-27 22:23:04 +0200

    Initial styling TypesOfMeals.css

--- src/pages/cook/meals/breakfast/BreakfastList.tsx
+++ src/pages/cook/meals/breakfast/BreakfastList.tsx
@@ -1,32 +1,36 @@
-import { useContext, useEffect} from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import "../TypesOfMeals.css";
+import breakfast_icon from "../../../../icons/breakfast_icon.png";
+
 import { MealsContext } from "../../../../context/MealsContext";
 
 import { MealIngredient } from "../../../../types/type";
 
 const BreakfastList = () => {
-    const navigate = useNavigate()
-    const { breakfastList }: any = useContext(MealsContext);
-    const displayingBreakfastList = breakfastList.map((doc: MealIngredient) => (
-        <div key={doc.id}>
-          <h3>
-            {doc.title} - {doc.amount}
-          </h3>
-        </div>
-      ));
-    
+  const navigate = useNavigate();
+  const { breakfastList }: any = useContext(MealsContext);
+  const displayingBreakfastList = breakfastList.map((doc: MealIngredient) => (
+    <div className="typesOfMeals-list" key={doc.id}>
+      <p>{doc.title}</p>
+      <p>{doc.amount}</p>
+    </div>
+  ));
 
-    const addMoreIngredientsToBreakfast = () => {
-        navigate("/cook/breakfast/ingredients")
-      };
-
+  const addMoreIngredientsToBreakfast = () => {
+    navigate("/cook/breakfast/ingredients");
+  };
 
   return (
-    <div>     
-    {displayingBreakfastList}
-    <button onClick={addMoreIngredientsToBreakfast}>Add more</button>
-  </div>
-  )
-}
+    <div className="typesOfMeals-container">
+      <div className="typesOfMeals-header">
+        <img src={breakfast_icon} alt="breakfast icon" />
+      </div>
+      {displayingBreakfastList}
+      <button  onClick={addMoreIngredientsToBreakfast}>Add more</button>
+    </div>
+  );
+};
 
-export default BreakfastList
+export default BreakfastList;
