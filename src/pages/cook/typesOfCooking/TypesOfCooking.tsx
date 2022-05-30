80acbae821130c85bd01313774005c9c0c411ea2
koronagaba <killleon007@gmail.com>
2022-05-30 12:18:19 +0200

    Fixed conflict in TypesOfCooking

--- src/pages/cook/typesOfCooking/TypesOfCooking.tsx
+++ src/pages/cook/typesOfCooking/TypesOfCooking.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
+import "./TypesOfCooking.css";
 import add_circle from "../../../icons/add_circle.svg";
 import favorite from "../../../icons/favorite.svg";
 import recipe from "../../../icons/recipe.png";
@@ -9,29 +10,37 @@ import ingredients from "../../../icons/ingredients.png";
 const TypesOfCooking = () => {
   return (
     <div className="meal-types-of-cooking">
-            <Link to={"/cook/breakfast/ingredients"}>
-              <img
-                src={ingredients}
-                alt="food (ingredients)"
-                className="meal-ingredients"
-              />
-            </Link>
-            {/* <Link to={"/cook/breakfast/recipeList"}>
-              <img src={recipe} alt="recipes" className="meal-recipes" />
-            </Link>
+      <Link to={"/cook/breakfast/ingredients"}>
+        <img
+          src={ingredients}
+          alt="food (ingredients)"
+          className="meal-ingredients"
+        />
+      </Link>
 
-            <Link to={"/cook/breakfast/favoriteFood"}>
-              <img src={favorite} alt="favorite" className="meal-favorite" />
-            </Link>
-            <Link to={"/cook/breakfast/createRecipe"}>
-              <img
-                src={add_circle}
-                alt="create recipe"
-                className="meal-create-recipe"
-              />
-            </Link> */}
-          </div>
-  )
-}
+      <div className="disabled">
+        <Link to={""}>
+          {/* /cook/breakfast/recipeList */}
+          <img src={recipe} alt="recipes" className="meal-recipes" />
+          <p className="textHide">This functionality will be activated soon</p>
+        </Link>
+        <Link to={""}>
+          {/* /cook/breakfast/favoriteFood */}
+          <img src={favorite} alt="favorite" className="meal-favorite" />
+          <p className="textHide">this functionality will be activated soon</p>
+        </Link>
+        <Link to={""}>
+          {/* /cook/breakfast/createRecipe */}
+          <img
+            src={add_circle}
+            alt="create recipe"
+            className="meal-create-recipe"
+          />
+          <p className="textHide">this functionality will be activated soon</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default TypesOfCooking
+export default TypesOfCooking;
