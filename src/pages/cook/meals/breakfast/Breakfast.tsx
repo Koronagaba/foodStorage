4983fc46e3f9694e2d26bb93de9bce61f98f05a8
koronagaba <killleon007@gmail.com>
2022-05-30 15:37:20 +0200

    Improvement arrowBack routing in Breakfast.tsx

--- src/pages/cook/meals/breakfast/Breakfast.tsx
+++ src/pages/cook/meals/breakfast/Breakfast.tsx
@@ -1,75 +1,68 @@
-import { useContext, useRef, SyntheticEvent, useState, useEffect } from "react";
-import { NavLink, Link, useNavigate, Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { Link, useNavigate, Outlet, Routes, Route } from "react-router-dom";
 
 import "../Meal.css";
 import arrow_back from "../../../../icons/arrow_back.svg";
 
-
-
-
-import TypesOfCooking from "../../typesOfCooking/TypesOfCooking";
-import Ingredients from "../../typesOfCooking/ingredients/Ingredients";
 import { MealsContext } from "../../../../context/MealsContext";
-import BreakfastList from "./BreakfastList";
 import SearchBarMeal from "../SearchBarMeal";
 
 const Breakfast = () => {
-
- 
   const { breakfastList }: any = useContext(MealsContext);
-
 
   const navigate = useNavigate();
 
-  const handleArrowBack = () => {
-    if(breakfastList){
-    navigate("/cook/breakfast/breakfastList")
-    }else{
-      navigate("/cook/breakfast/ingredients")
-    }
-  }
-
-  useEffect(()=>{
+  useEffect(() => {
     if (breakfastList) {
-    navigate('breakfastList')
-    }else {
-    navigate('ingredients')
+      navigate("breakfastList");
+    } else {
+      navigate("ingredients");
     }
-  },[])
+  }, []);
 
+  const handleArrowBackToBreakfast = () => {
+    if (breakfastList) {
+      navigate("/cook/breakfast/breakfastList");
+    } else {
+      navigate("cook");
+    }
+  };
 
+  const BackToCook = () => {
+    return (
+      <img
+        onClick={() => navigate("cook")}
+        src={arrow_back}
+        alt="arrow back"
+        className="arrow-back"
+      />
+    );
+  };
+
+  const BackToBreakfast = () => {
+    return (
+      <img
+        onClick={handleArrowBackToBreakfast}
+        src={arrow_back}
+        alt="arrow back"
+        className="arrow-back"
+      />
+    );
+  };
 
   return (
     <div className="meal-container">
       <div className="meal">
         <div className="meal-header">
-          <img
-            onClick={handleArrowBack}
-            src={arrow_back}
-            alt="arrow back"
-            className="arrow-back"
-          />
+          <Routes>
+            <Route path="breakfastList" element={<BackToCook />} />
+            <Route path="ingredients" element={<BackToBreakfast />} />
+          </Routes>
           <Link to={"/cook"} className="meal-title">
             <h2>Breakfast</h2>
           </Link>
         </div>
         <SearchBarMeal />
-        
-        {/* <Condit /> */}
-        {/* {breakfastList ? (
-          <>
-            <div>Breakfast's products list:</div>
-            {displayingBreakfastList}
-            <button onClick={addMoreIngredientsToBreakfast}>Add more</button>
-          </>
-        ) : (
-          <TypesOfCooking />
-        )} */}
-
-        {/* {breakfastList && showTypesOfCooking } */}
-
-        {/* <div className="nav-sorting"></div> */}
-
         <div>
           <Outlet />
         </div>
