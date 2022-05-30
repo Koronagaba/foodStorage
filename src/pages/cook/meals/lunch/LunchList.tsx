93ca5eae2309c49fb599bc208c3370fabc22c6ab
koronagaba <killleon007@gmail.com>
2022-05-30 17:52:50 +0200

    display a lunchList

--- src/pages/cook/meals/lunch/LunchList.tsx
+++ src/pages/cook/meals/lunch/LunchList.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useContext } from "react";
+import { MealsContext } from "../../../../context/MealsContext";
+import { MealIngredient } from "../../../../types/type";
 
 const LunchList = () => {
-  return (
-    <div>LunchList</div>
-  )
-}
+  const { lunchList }: any = useContext(MealsContext);
 
-export default LunchList
+  const displayLunchList = lunchList.map((doc: MealIngredient) => (
+    <div className="typesOfMeals-list" key={doc.id}>
+      <p>{doc.title}</p>
+      <p>{doc.amount}</p>
+    </div>
+  ));
+
+  return <>{displayLunchList}</>;
+};
+
+export default LunchList;
