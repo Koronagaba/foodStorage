ed038991caec8265933cbd695a692f23a9bd71eb
koronagaba <killleon007@gmail.com>
2022-05-31 12:00:59 +0200

    Create reusable component 'Meal.tsx' that will replace components: 'Breakfast.tsx, Lunch.tsx, Supper.tsx, Snack.tsx'

---
+++ src/pages/cook/meals/Meal.tsx
@@ -0,0 +1,54 @@
+import { useEffect, FC } from "react";
+import { Link, useNavigate, Outlet, Routes, Route } from "react-router-dom";
+
+import "./Meal.css";
+
+import SearchBarMeal from "./SearchBarMeal";
+import { BackToCook, BackToMealList } from "./consts/ArrowBack";
+
+interface MealProps {
+    title: string,
+    backToCook: string,
+    backToMealList: string,
+    navToList: string,
+    nameOfCollection: any
+}
+
+const Meal:FC <MealProps> = ({title, backToCook, backToMealList, navToList, nameOfCollection }) => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+      console.log(nameOfCollection);
+      
+    if (nameOfCollection.length) {
+      navigate(navToList);
+    } else {
+      navigate("ingredients");
+    }
+  }, []);
+
+  return (
+    <div className="meal-container">
+      <div className="meal">
+        <div className="meal-header">
+          <Routes>
+            <Route path={`${backToCook}`} element={<BackToCook />} />
+            <Route
+              path="ingredients"
+              element={<BackToMealList path={backToMealList} />}
+            />
+          </Routes>
+          <Link to={"/cook"} className="meal-title">
+            <h2>{title}</h2>
+          </Link>
+        </div>
+        <SearchBarMeal />
+        <div>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Meal;
