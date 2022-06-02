58e39b45363937f8ba7fe19aa9e795c878715070
koronagaba <killleon007@gmail.com>
2022-06-02 14:02:21 +0200

    Create hook to sort 'useSort.tsx'

---
+++ src/hooks/useSort.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Product } from "../types/type";
+
+const useSort = () => {
+
+
+    const sortTitle = (a:Product, b: Product) => {
+        if (a.title.toLowerCase() < b.title.toLowerCase()) return -1;
+        if (a.title.toLowerCase() > b.title.toLowerCase()) return 1;
+        return 0;
+      };
+    return { sortTitle }
+}
+
+export default useSort
