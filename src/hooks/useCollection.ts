f308c8ad56e6fa7c33e76edddbaa3dd58ae94580
koronagaba <killleon007@gmail.com>
2022-06-03 15:01:24 +0200

    Pass second parameter 'order' to the 'useCollection.ts' to sort collection

--- src/hooks/useCollection.ts
+++ src/hooks/useCollection.ts
@@ -3,23 +3,23 @@ import { db } from "../firebase/config";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { Product, ShopProduct } from "../types/type";
 
-export type CollectionType = Product | ShopProduct
+export type CollectionType = Product | ShopProduct;
 
-export const useCollection = <T extends CollectionType>(c: string) => {
+export const useCollection = <T extends CollectionType>(c: string, order: string) => {
   const [documents, setDocuments] = useState<T[]>([]);
 
   useEffect(() => {
-  const ref = collection(db, c);
+    const ref = collection(db, c);
 
-  const q = query(ref, orderBy("title"));  
-   const unsubscribe = onSnapshot(q, (doc) => {
-       const results: T[] = [];   
-       doc.forEach((item) => {
-       const data = {...item.data(), id: item.id} as T
-       results.push(data);
-     });
-     setDocuments(results);
-   });
- }, [c]);
+    const q = query(ref, orderBy(order));
+    onSnapshot(q, (doc) => {
+      const results: T[] = [];
+      doc.forEach((item) => {
+        const data = { ...item.data(), id: item.id } as T;
+        results.push(data);
+      });
+      setDocuments(results);
+    });
+  }, [c, order]);
   return { documents };
 };
