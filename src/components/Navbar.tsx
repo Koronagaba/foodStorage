ddc7b18d47d85867ee4a5ba88a8659fa0cb81007
koronagaba <killleon007@gmail.com>
2022-06-19 22:06:10 +0200

    Close drop-down menu when we click outside component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import './Navbar.css';
 
 const Navbar = () => {
   const [stickyStyle, setStickyStyle] = useState('navbar');
-  const [toggleClass, setToggleClass] = useState(false);
+  const [toggleOptions, setToggleOptions] = useState(false);
 
+  const ref: any = useRef()
   const activeLink = ({ isActive }: any) => ({ color: isActive ? '#fff' : '' });
 
   const handleScroll = () => {
@@ -22,36 +23,74 @@ const Navbar = () => {
   });
 
   const handleToggle = () => {
-    setToggleClass(!toggleClass);
+    setToggleOptions(!toggleOptions);
   };
 
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e:any) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (toggleOptions && ref.current && !ref.current.contains(e.target)) {
+        setToggleOptions(false)
+      }
+    }
+
+    document.addEventListener("mousedown", checkIfClickedOutside)
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside)
+    }
+  }, [toggleOptions])
+ 
+
+
   return (
-    <div className={`navigation ${stickyStyle}`}>
+    <div  className={`navigation ${stickyStyle}`}>
       <nav>
         <Link to={'/cook'} className="logo">
           Let's cook with koro!
         </Link>
+        <div ref={ref} >
         <div onClick={handleToggle} className="hamburger-icon">
-          <div className={`icon-1 ${toggleClass ? 'icon-a' : null}`}></div>
-          <div className={`icon-2 ${toggleClass ? 'icon-b' : null} `}></div>
-          <div className={`icon-3 ${toggleClass ? 'icon-c' : null} `}></div>
+          <div className={`icon-1 ${toggleOptions ? 'icon-a' : null}`}></div>
+          <div className={`icon-2 ${toggleOptions ? 'icon-b' : null} `}></div>
+          <div className={`icon-3 ${toggleOptions ? 'icon-c' : null} `}></div>
           <div className="clear"></div>
         </div>
-        <div className={`options ${toggleClass ? 'show-options' : null} `}>
+        <div className={`options ${toggleOptions ? 'show-options' : null} `}>
           <div className="inside-options">
-            <NavLink to={'/cook'} style={activeLink}>
+            <NavLink
+              to={'/cook'}
+              style={activeLink}
+              onClick={() => setToggleOptions(false)}
+            >
               Cook
             </NavLink>
-            <NavLink to={'/createNewProduct'} style={activeLink}>
+            <NavLink
+              to={'/createNewProduct'}
+              style={activeLink}
+              onClick={() => setToggleOptions(false)}
+            >
               Create
             </NavLink>
-            <NavLink to={'/stock'} style={activeLink}>
+            <NavLink
+              to={'/stock'}
+              style={activeLink}
+              onClick={() => setToggleOptions(false)}
+            >
               Stock
             </NavLink>
-            <NavLink to={'/shoppingList'} style={activeLink}>
+            <NavLink
+              to={'/shoppingList'}
+              style={activeLink}
+              onClick={() => setToggleOptions(false)}
+            >
               Shopping List
             </NavLink>
           </div>
+        </div>
         </div>
       </nav>
     </div>
