5cdd334cf3a8c77098b433cff6b986a923d9387f
koronagaba <killleon007@gmail.com>
2022-07-04 20:38:39 +0200

    Save user theme  preferences using localStorage

--- src/components/darkMode/DarkMode.tsx
+++ src/components/darkMode/DarkMode.tsx
@@ -1,10 +1,30 @@
+
 import './DarkMode.css';
 
 const DarkMode = () => {
 
+  const body = document.body;
+
+  let storageTheme = localStorage.getItem('theme');
+
   const toggleTheme = () => {
-    document.body.classList.toggle('dark')
+    if (storageTheme === 'dark') {
+      body.classList.remove('dark');
+      storageTheme = "light"
+    } else {
+      body.classList.add('dark');
+      storageTheme = "dark"
+    }
+
+    localStorage.setItem('theme', storageTheme)
+  };
+
+
+  if (storageTheme === 'dark') {
+    body.classList.add('dark');
   }
+
+ 
 
   return (
     <button onClick={toggleTheme} className="toggle-theme">
