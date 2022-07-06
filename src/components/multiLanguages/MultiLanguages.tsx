57a3b4e1678c4693e4ac0d3f105eea60e7ef46cd
koronagaba <killleon007@gmail.com>
2022-07-06 07:38:16 +0200

    Check localStorage - key='i18nextLng' to select flag icon

--- src/components/multiLanguages/MultiLanguages.tsx
+++ src/components/multiLanguages/MultiLanguages.tsx
@@ -1,13 +1,16 @@
-import { useContext } from 'react';
-import { TranslateContext } from '../../context/TranslationContext';
+// import { useContext } from 'react';
+// import { TranslateContext } from '../../context/TranslationContext';
 import { useTranslation } from 'react-i18next';
 
 import './MultiLanguages.css';
 
 const MultiLanguages = () => {
-  const { isEnglish, setIsEnglish } = useContext(TranslateContext);
+  // const { isEnglish, setIsEnglish } = useContext(TranslateContext);
 
   const { i18n } = useTranslation();
+
+  const i18nextLecalStorage = localStorage.getItem('i18nextLng');
+
 
   return (
     <div className="multi-languages">
@@ -17,20 +20,20 @@ const MultiLanguages = () => {
         height="16"
         alt="Poland"
         onClick={() => {
-          setIsEnglish(false);
+          // setIsEnglish(false);
           i18n.changeLanguage('pl');
         }}
-        className={!isEnglish ? 'disabled' : ''}
+        className={i18nextLecalStorage === 'pl' ? 'disabled' : ''}
       />
       <img
         src="https://flagcdn.com/gb.svg"
         width="30"
         alt="United Kingdom"
         onClick={() => {
-          setIsEnglish(true);
+          // setIsEnglish(true);
           i18n.changeLanguage('en');
         }}
-        className={isEnglish ? 'disabled' : ''}
+        className={i18nextLecalStorage === 'en' ? 'disabled' : ''}
       />
     </div>
   );
