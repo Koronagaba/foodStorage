445b2aa7c68d36f8e774ea442ba5b2e7ec01467e
koronagaba <killleon007@gmail.com>
2022-07-09 12:05:44 +0200

    Merged and addition amount value shoppingList && stockProductsList

--- src/pages/stock/ProductsList.tsx
+++ src/pages/stock/ProductsList.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import { useContext } from 'react';
 
 import SingleStockProduct from './SingleStockProduct';
 
-import { StockProduct } from '../../types/type';
+import { StockProduct, ShoppingListProduct, MergedStockProdAndShopProd } from '../../types/type';
+import { FoodStorageContext } from '../../context/FoodStorageContext';
 
 interface ProductsProps {
   stockProductsList: StockProduct[];
@@ -13,14 +14,39 @@ const ProductsList: React.FC<ProductsProps> = ({
   stockProductsList,
   searchStock,
 }) => {
+  const { shoppingList } = useContext(FoodStorageContext);
+
   const filterMatchedProducts = (product: StockProduct) => {
     return product.title.toLowerCase().includes(searchStock.toLowerCase());
   };
 
+
+  const mergedList: MergedStockProdAndShopProd [] = []
+
+  stockProductsList.forEach((stockProd: StockProduct) => {
+    shoppingList.forEach((shopProd: ShoppingListProduct) => {
+      if(stockProd.title === shopProd.title){
+          const indexMergedList = mergedList.findIndex(({title}) => (
+              title === stockProd.title
+          ))
+          if(indexMergedList === -1){
+              mergedList.push({
+                title: shopProd.title,
+                amount: shopProd.amount,
+                quantity: shopProd.amount
+              })
+          }else{  
+            mergedList[indexMergedList].quantity = mergedList[indexMergedList].quantity + shopProd.amount
+          }
+          return mergedList
+      }
+    });
+  });
+
   return (
     <>
       {stockProductsList.filter(filterMatchedProducts).map((product) => (
-        <SingleStockProduct key={product.id} product={product} />
+        <SingleStockProduct key={product.id} product={product} mergedList={mergedList}/>
       ))}
     </>
   );
