1a0d94121f4c166f6d0b42717e6b5651b52e5e98
koronagaba <killleon007@gmail.com>
2022-07-10 18:51:51 +0200

    Working at displaying shoppingList product amount in Stock Product

--- src/pages/stock/ProductsList.tsx
+++ src/pages/stock/ProductsList.tsx
@@ -2,7 +2,12 @@ import { useContext } from 'react';
 
 import SingleStockProduct from './SingleStockProduct';
 
-import { StockProduct, ShoppingListProduct, MergedStockProdAndShopProd } from '../../types/type';
+import {
+  StockProduct,
+  ShoppingListProduct,
+  MergedStockProdAndShopProd,
+  NewStockProduct,
+} from '../../types/type';
 import { FoodStorageContext } from '../../context/FoodStorageContext';
 
 interface ProductsProps {
@@ -20,34 +25,71 @@ const ProductsList: React.FC<ProductsProps> = ({
     return product.title.toLowerCase().includes(searchStock.toLowerCase());
   };
 
-
-  const mergedList: MergedStockProdAndShopProd [] = []
+  const mergedList: MergedStockProdAndShopProd[] = [];
 
   stockProductsList.forEach((stockProd: StockProduct) => {
     shoppingList.forEach((shopProd: ShoppingListProduct) => {
-      if(stockProd.title === shopProd.title){
-          const indexMergedList = mergedList.findIndex(({title}) => (
-              title === stockProd.title
-          ))
-          if(indexMergedList === -1){
-              mergedList.push({
-                title: shopProd.title,
-                amount: shopProd.amount,
-                quantity: shopProd.amount
-              })
-          }else{  
-            mergedList[indexMergedList].quantity = mergedList[indexMergedList].quantity + shopProd.amount
-          }
-          return mergedList
+      if (stockProd.title === shopProd.title) {
+        const indexMergedList = mergedList.findIndex(
+          ({ title }) => title === stockProd.title
+        );
+        if (indexMergedList === -1) {
+          mergedList.push({
+            title: shopProd.title,
+            amount: shopProd.amount,
+            quantity: shopProd.amount,
+          });
+        } else {
+          mergedList[indexMergedList].quantity =
+            mergedList[indexMergedList].quantity + shopProd.amount;
+        }
+        return mergedList;
       }
     });
   });
 
+  // const display = stockProductsList.filter(filterMatchedProducts).map((product) => (
+  //   <SingleStockProduct key={product.id} product={product} />
+  // ))
+
+  const newStockList: NewStockProduct[] = [];
+
+  stockProductsList.filter(filterMatchedProducts).forEach((product) => {
+    mergedList.forEach((mergedProds: MergedStockProdAndShopProd) => {
+      if (mergedProds.title === product.title) {
+          newStockList.push({
+            title: product.title,
+            amount: product.amount,
+            shoppingListAmount: mergedProds.quantity,
+            id: product.id,
+          });  
+
+      } else {
+        newStockList.push({
+          title: product.title,
+          amount: product.amount,
+          shoppingListAmount: 0,
+          id: product.id,
+        });
+      }
+      console.log(newStockList);
+    });
+    return newStockList;
+
+    
+  });
+
+  const displayStockProducts = newStockList.map((product: NewStockProduct) => {
+    return <SingleStockProduct key={product.id} product={product} />;
+  });
+
   return (
     <>
-      {stockProductsList.filter(filterMatchedProducts).map((product) => (
+      {displayStockProducts}
+      {/* {displayStockProducts} */}
+      {/* {stockProductsList.filter(filterMatchedProducts).map((product) => (
         <SingleStockProduct key={product.id} product={product} mergedList={mergedList}/>
-      ))}
+      ))} */}
     </>
   );
 };
