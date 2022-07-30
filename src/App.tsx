5f09f69830f7aba6a8778c1e65a19f73fcfb93a2
koronagaba <killleon007@gmail.com>
2022-07-30 13:35:43 +0200

    Dispaly HandleRemove button only when athname is qual to 'cook'

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate, useLocation } from 'react-router-dom';
 import { useState, useContext } from 'react';
 import { useTranslation } from 'react-i18next';
 
@@ -26,167 +26,169 @@ import Meal from './pages/cook/meals/meal/Meal';
 import MealList from './pages/cook/meals/mealList/MealList';
 import MultiLanguages from './components/multiLanguages/MultiLanguages';
 import DarkMode from './components/darkMode/DarkMode';
+import HandleRemoval from './components/handleRemoval/HandleRemoval';
 
 function App() {
   const { breakfastList, lunchList, supperList, snackList } =
     useContext(MealsContext);
   const [toggleModal, setToggleModal] = useState(false);
 
+  useLocation();
+
   const { t } = useTranslation();
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <div className="darkMode-language-container">
-          <div className="darkMode-language-inside">
-            <DarkMode />
-            <MultiLanguages />
-          </div>
+      <Navbar />
+      <div className="darkMode-language-container">
+        <div className="darkMode-language-inside">
+          <DarkMode />
+          {window.location.pathname === '/cook' ? <HandleRemoval /> : null}
+          <MultiLanguages />
         </div>
-        <Routes>
-          <Route path="/cook" element={<Cook />}></Route>
-          <Route path="*" element={<Navigate to="/cook" />} />
-          {/* *********************************** */}
+      </div>
+      <Routes>
+        <Route path="/cook" element={<Cook />}></Route>
+        <Route path="*" element={<Navigate to="/cook" />} />
+        {/* *********************************** */}
+        <Route
+          path="/cook/breakfast"
+          element={
+            <Meal
+              title={t('breakfast')}
+              backToCook={'/*'}
+              backToMealList={'breakfast'}
+              navToList={'breakfastList'}
+              collection={breakfastList}
+            />
+          }
+        >
           <Route
             path="/cook/breakfast"
             element={
-              <Meal
-                title={t('breakfast')}
-                backToCook={'/*'}
-                backToMealList={'breakfast'}
-                navToList={'breakfastList'}
+              <MealList
                 collection={breakfastList}
+                iconName={breakfast_icon}
+                altProp={'breakfast icon'}
+                path={'breakfast'}
               />
             }
-          >
-            <Route
-              path="/cook/breakfast"
-              element={
-                <MealList
-                  collection={breakfastList}
-                  iconName={breakfast_icon}
-                  altProp={'breakfast icon'}
-                  path={'breakfast'}
-                />
-              }
-            ></Route>
-            <Route
-              path="ingredients"
-              element={<Ingredients nameOfCollection={'breakfast'} />}
-            ></Route>
-            <Route path="recipeList" element={<RecipeList />}></Route>
-            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-            <Route path="createRecipe" element={<CreateRecipe />} />
-          </Route>
-          {/* ******************************************** */}
+          ></Route>
+          <Route
+            path="ingredients"
+            element={<Ingredients nameOfCollection={'breakfast'} />}
+          ></Route>
+          <Route path="recipeList" element={<RecipeList />}></Route>
+          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+          <Route path="createRecipe" element={<CreateRecipe />} />
+        </Route>
+        {/* ******************************************** */}
+        <Route
+          path="/cook/lunch"
+          element={
+            <Meal
+              title={t('lunch')}
+              backToCook={'/*'}
+              backToMealList={'lunch'}
+              navToList={'lunchList'}
+              collection={lunchList}
+            />
+          }
+        >
           <Route
             path="/cook/lunch"
             element={
-              <Meal
-                title={t('lunch')}
-                backToCook={'/*'}
-                backToMealList={'lunch'}
-                navToList={'lunchList'}
+              <MealList
                 collection={lunchList}
+                iconName={lunch_icon}
+                altProp={'lunch icon'}
+                path={'lunch'}
               />
             }
-          >
-            <Route
-              path="/cook/lunch"
-              element={
-                <MealList
-                  collection={lunchList}
-                  iconName={lunch_icon}
-                  altProp={'lunch icon'}
-                  path={'lunch'}
-                />
-              }
-            ></Route>
-            <Route
-              path="ingredients"
-              element={<Ingredients nameOfCollection={'lunch'} />}
-            ></Route>
-            <Route path="recipeList" element={<RecipeList />}></Route>
-            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-            <Route path="createRecipe" element={<CreateRecipe />} />
-          </Route>
-          {/* *************************************************** */}
+          ></Route>
+          <Route
+            path="ingredients"
+            element={<Ingredients nameOfCollection={'lunch'} />}
+          ></Route>
+          <Route path="recipeList" element={<RecipeList />}></Route>
+          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+          <Route path="createRecipe" element={<CreateRecipe />} />
+        </Route>
+        {/* *************************************************** */}
+        <Route
+          path="/cook/supper"
+          element={
+            <Meal
+              title={t('supper')}
+              backToCook={'/*'}
+              backToMealList={'supper'}
+              navToList={'supperList'}
+              collection={supperList}
+            />
+          }
+        >
           <Route
             path="/cook/supper"
             element={
-              <Meal
-                title={t('supper')}
-                backToCook={'/*'}
-                backToMealList={'supper'}
-                navToList={'supperList'}
+              <MealList
                 collection={supperList}
+                iconName={salad_icon}
+                altProp={'salad icon'}
+                path={'supper'}
               />
             }
-          >
-            <Route
-              path="/cook/supper"
-              element={
-                <MealList
-                  collection={supperList}
-                  iconName={salad_icon}
-                  altProp={'salad icon'}
-                  path={'supper'}
-                />
-              }
-            ></Route>
-            <Route
-              path="ingredients"
-              element={<Ingredients nameOfCollection={'supper'} />}
-            ></Route>
-            <Route path="recipeList" element={<RecipeList />}></Route>
-            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-            <Route path="createRecipe" element={<CreateRecipe />} />
-          </Route>
+          ></Route>
+          <Route
+            path="ingredients"
+            element={<Ingredients nameOfCollection={'supper'} />}
+          ></Route>
+          <Route path="recipeList" element={<RecipeList />}></Route>
+          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+          <Route path="createRecipe" element={<CreateRecipe />} />
+        </Route>
 
-          {/* *************************************************** */}
+        {/* *************************************************** */}
+        <Route
+          path="/cook/snack"
+          element={
+            <Meal
+              title={t('snacks')}
+              backToCook={'/*'}
+              backToMealList={'snack'}
+              navToList={'snackList'}
+              collection={snackList}
+            />
+          }
+        >
           <Route
             path="/cook/snack"
             element={
-              <Meal
-                title={t('snacks')}
-                backToCook={'/*'}
-                backToMealList={'snack'}
-                navToList={'snackList'}
+              <MealList
                 collection={snackList}
+                iconName={snack_icon}
+                altProp={'snack icon'}
+                path={'snack'}
               />
             }
-          >
-            <Route
-              path="/cook/snack"
-              element={
-                <MealList
-                  collection={snackList}
-                  iconName={snack_icon}
-                  altProp={'snack icon'}
-                  path={'snack'}
-                />
-              }
-            ></Route>
-            <Route
-              path="ingredients"
-              element={<Ingredients nameOfCollection={'snack'} />}
-            ></Route>
-            <Route path="recipeList" element={<RecipeList />}></Route>
-            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-            <Route path="createRecipe" element={<CreateRecipe />} />
-          </Route>
-          {/* *********************************************************** */}
-          {/* <Route
+          ></Route>
+          <Route
+            path="ingredients"
+            element={<Ingredients nameOfCollection={'snack'} />}
+          ></Route>
+          <Route path="recipeList" element={<RecipeList />}></Route>
+          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+          <Route path="createRecipe" element={<CreateRecipe />} />
+        </Route>
+        {/* *********************************************************** */}
+        {/* <Route
             path="/createNewProduct"
             element={<CreateProduct setToggleModal={setToggleModal} />}
           /> */}
-          <Route path="/stock" element={<Stock />}>
-            <Route path="/stock/createProduct" element={<Stock />} />
-          </Route>
-          <Route path="/shoppingList" element={<ShoppingList />} />
-          {/* <Route path="*" element={< NoMatch/>} /> */}
-        </Routes>
-      </BrowserRouter>
+        <Route path="/stock" element={<Stock />}>
+          <Route path="/stock/createProduct" element={<Stock />} />
+        </Route>
+        <Route path="/shoppingList" element={<ShoppingList />} />
+        {/* <Route path="*" element={< NoMatch/>} /> */}
+      </Routes>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom'
+
 
 import './i18n';
 
@@ -13,7 +15,9 @@ ReactDOM.render(
     <FoodStorageContextProvider>
       <SearchContextProvider>
         <MealsContextProvider>
+          <BrowserRouter>
           <App />
+          </BrowserRouter>
         </MealsContextProvider>
       </SearchContextProvider>
     </FoodStorageContextProvider>
