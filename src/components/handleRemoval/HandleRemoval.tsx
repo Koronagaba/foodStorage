4bde591cf5ff882f688595a2ab2257899b8d8c4a
koronagaba <killleon007@gmail.com>
2022-08-01 15:35:36 +0200

    Button operation - Delete all of products

--- src/components/handleRemoval/HandleRemoval.tsx
+++ src/components/handleRemoval/HandleRemoval.tsx
@@ -1,13 +1,31 @@
-import { useTranslation } from "react-i18next"
-import './HandleRemoval.css'
+import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+import { MealsContext } from '../../../src/context/MealsContext';
+import { deleteMidnight } from '../../hooks/useDelete';
+
+import './HandleRemoval.css';
 
 const HandleRemoval = () => {
-    const { t } = useTranslation()
+  const { breakfastList, lunchList, supperList, snackList }: any =
+    useContext(MealsContext);
+
+  const deleteEveryDay = () => {
+    deleteMidnight(breakfastList, 'breakfast');
+    deleteMidnight(lunchList, 'lunch');
+    deleteMidnight(supperList, 'supper');
+    deleteMidnight(snackList, 'snack');
+  };
+
+  const deleteAllProducts = () => {
+    deleteEveryDay();
+  };
+
+  const { t } = useTranslation();
   return (
     <div className="handle-removal">
-        <button>{t('delete_everything')}</button>
+      <button onClick={deleteAllProducts}>{t('delete_everything')}</button>
     </div>
-  )
-}
+  );
+};
 
-export default HandleRemoval
+export default HandleRemoval;
--- src/hooks/useDelete.ts
+++ src/hooks/useDelete.ts
@@ -1,11 +1,9 @@
-// import { db } from '../firebase/config'
-// import { doc, deleteDoc } from 'firebase/firestore'
+import { db } from '../../src/firebase/config'
+import { doc, deleteDoc } from 'firebase/firestore'
+import { MealItem } from '../../src/context/MealsContext'
 
-// export const useDelete = async () => {
 
-//     const deleteProduct = (collection: string, id: string) => {
-//         deleteDoc(doc(db, collection, id))
-//     }
+export const deleteMidnight = (mealList: MealItem[], collection: string ) => {  
+  mealList.map(({ id }: any) => deleteDoc(doc(db, collection, id)));  
+}
 
-//     return {  deleteProduct }
-// }
