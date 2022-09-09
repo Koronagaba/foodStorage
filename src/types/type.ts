526a3dc18e108928dbfe24558538b19a6f1b6da6
koronagaba <killleon007@gmail.com>
2022-09-09 08:34:50 +0200

    Typescript - new optional property in SingleHistoryOfCooking interface

--- src/types/type.ts
+++ src/types/type.ts
@@ -6,8 +6,15 @@ export interface StockProduct {
 }
 
 export interface At {
-  nanoseconds: number;
+  // nanoseconds: number | string;
+  nanoseconds: number ;
+  // seconds: number | string;
   seconds: number;
+}
+
+export interface Date {
+  date: string,
+  atTime: string
 }
 
 export interface ShoppingListProduct {
@@ -50,16 +57,10 @@ export interface SingleHistoryOfCooking {
   amount: number;
   createdAt: At;
   nameOfMeal: string;
+  id: string;
+  date?: Date
 }
 
-// export type SingleItemProps = {
-//   item: ShopList;
-//   toggleEdit: (id: number, itemTitle: string, itemAmount: number, itemIsEditing: boolean) => void;
-//   handleEdit: (a: number, b: string, c: number | undefined, d: number) => void; //1tttttttttttttttttttttt
-//   handleDelete: (a: number) => void;
-//   moveProductIntoBag: (a: number, b: string, c: number, d: boolean) => void;
-//   handleSendToStock: (a: string, b: number) => void;
-// };
 export type SingleShopProductProps = {
   productOfShoppingList: ShoppingListProduct;
   // toggleEdit: (id: number, itemTitle: string, itemAmount: number, itemIsEditing: boolean) => void;
@@ -68,33 +69,3 @@ export type SingleShopProductProps = {
   // moveProductIntoBag: (a: string, b: string, c: number, d: boolean) => void;
   // handleSendToStock: (a: string, b: number) => void;
 };
-
-// export interface ToggleEditProps {
-//   id: number | undefined;
-//   itemTitle: string;
-//   itemAmount: number;
-//   itemIsEditing: boolean;
-// }
-
-// export interface DeleteItemProps {
-//   id: number
-// }
-
-// export interface EditItemFromShoppingListProps {
-//   id: number;
-//   itemTitle: string;
-//   editAmount: number;
-//   itemAmount: number;
-// }
-
-// export interface MoveProductIntoBagProps {
-//   id: number;
-//   itemTitle: string;
-//   itemAmount: number;
-//   itemInBag: number;
-// }
-
-// export interface HandleSendToStockProps {
-//   itemTitle: string;
-//   itemAmount: number;
-// }
