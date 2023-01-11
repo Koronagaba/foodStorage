b8a99c8a0a9173ef54dd28dea3308431530c9b9a
Koronagaba <killleon007@gmail.com>
2023-01-11 11:02:40 +0100

    Scroll up when change the path location

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
 import { useContext } from 'react';
-import { useTranslation } from 'react-i18next';
 
 import { MealsContext } from './context/MealsContext';
 
@@ -36,195 +36,197 @@ function App() {
   const { breakfastList, lunchList, supperList, snackList } =
     useContext(MealsContext);
 
-  const { t } = useTranslation();
+
   return (
     <div className="App">
-      <Navbar />
-      <FunctionsBar />
-      <Routes>
-        <Route path="/cook" element={<Cook />}></Route>
-        <Route path="/history" element={<HistoryOfCooking />}>
-          <Route path="/history/month_:id" element={<MonthHistory />}></Route>
-          <Route path="/history/year_:id" element={<YearHistory />}></Route>
-          <Route
-            path="/history/rangeHistory_:id"
-            element={<RangeHistoryList />}
-          ></Route>
-        </Route>
-        <Route path="*" element={<Navigate to="/cook" />} />
-        {/* ***************** Breakfast ***************** */}
-        <Route
-          path="/cook/breakfast"
-          element={
-            <MealHeader
-              title={'breakfast'}
-              backToCook={'/*'}
-              nameOfMeal={'breakfast'}
-              mealCollection={breakfastList}
-            />
-          }
-        >
+      <ScrollToTop>
+        <Navbar />
+        <FunctionsBar />
+        <Routes>
+          <Route path="/cook" element={<Cook />}></Route>
+          <Route path="/history" element={<HistoryOfCooking />}>
+            <Route path="/history/month_:id" element={<MonthHistory />}></Route>
+            <Route path="/history/year_:id" element={<YearHistory />}></Route>
+            <Route
+              path="/history/rangeHistory_:id"
+              element={<RangeHistoryList />}
+            ></Route>
+          </Route>
+          <Route path="*" element={<Navigate to="/cook" />} />
+          {/* ***************** Breakfast ***************** */}
           <Route
             path="/cook/breakfast"
             element={
-              <UsedProductsToCook
-                mealCollection={breakfastList}
-                iconName={breakfast_icon}
-                altProp={'breakfast icon'}
-                path={'breakfast'}
-              />
-            }
-          ></Route>
-          <Route
-            path="/cook/breakfast/edit"
-            element={<EditSingleMealProduct nameOfMeal={'breakfast'} />}
-          />
-          <Route
-            path="ingredients"
-            element={
-              <Ingredients
-                nameOfMealCollection={'breakfast'}
+              <MealHeader
+                title={'breakfast'}
+                backToCook={'/*'}
+                nameOfMeal={'breakfast'}
                 mealCollection={breakfastList}
               />
             }
-          ></Route>
-          <Route path="recipeList" element={<RecipeList />}></Route>
-          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-          <Route path="createRecipe" element={<CreateRecipe />} />
-        </Route>
-        {/* ***************** Lunch ***************** */}
-        <Route
-          path="/cook/lunch"
-          element={
-            <MealHeader
-              title={'lunch'}
-              backToCook={'/*'}
-              nameOfMeal={'lunch'}
-              mealCollection={lunchList}
+          >
+            <Route
+              path="/cook/breakfast"
+              element={
+                <UsedProductsToCook
+                  mealCollection={breakfastList}
+                  iconName={breakfast_icon}
+                  altProp={'breakfast icon'}
+                  path={'breakfast'}
+                />
+              }
+            ></Route>
+            <Route
+              path="/cook/breakfast/edit"
+              element={<EditSingleMealProduct nameOfMeal={'breakfast'} />}
             />
-          }
-        >
+            <Route
+              path="ingredients"
+              element={
+                <Ingredients
+                  nameOfMealCollection={'breakfast'}
+                  mealCollection={breakfastList}
+                />
+              }
+            ></Route>
+            <Route path="recipeList" element={<RecipeList />}></Route>
+            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+            <Route path="createRecipe" element={<CreateRecipe />} />
+          </Route>
+          {/* ***************** Lunch ***************** */}
           <Route
             path="/cook/lunch"
             element={
-              <UsedProductsToCook
-                mealCollection={lunchList}
-                iconName={lunch_icon}
-                altProp={'lunch icon'}
-                path={'lunch'}
-              />
-            }
-          ></Route>
-          <Route
-            path="/cook/lunch/edit"
-            element={<EditSingleMealProduct nameOfMeal={'lunch'} />}
-          />
-          <Route
-            path="ingredients"
-            element={
-              <Ingredients
-                nameOfMealCollection={'lunch'}
+              <MealHeader
+                title={'lunch'}
+                backToCook={'/*'}
+                nameOfMeal={'lunch'}
                 mealCollection={lunchList}
               />
             }
-          ></Route>
-          <Route path="recipeList" element={<RecipeList />}></Route>
-          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-          <Route path="createRecipe" element={<CreateRecipe />} />
-        </Route>
-        {/* ***************** Supper***************** */}
-        <Route
-          path="/cook/supper"
-          element={
-            <MealHeader
-              title={'supper'}
-              backToCook={'/*'}
-              nameOfMeal={'supper'}
-              mealCollection={supperList}
+          >
+            <Route
+              path="/cook/lunch"
+              element={
+                <UsedProductsToCook
+                  mealCollection={lunchList}
+                  iconName={lunch_icon}
+                  altProp={'lunch icon'}
+                  path={'lunch'}
+                />
+              }
+            ></Route>
+            <Route
+              path="/cook/lunch/edit"
+              element={<EditSingleMealProduct nameOfMeal={'lunch'} />}
             />
-          }
-        >
+            <Route
+              path="ingredients"
+              element={
+                <Ingredients
+                  nameOfMealCollection={'lunch'}
+                  mealCollection={lunchList}
+                />
+              }
+            ></Route>
+            <Route path="recipeList" element={<RecipeList />}></Route>
+            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+            <Route path="createRecipe" element={<CreateRecipe />} />
+          </Route>
+          {/* ***************** Supper***************** */}
           <Route
             path="/cook/supper"
             element={
-              <UsedProductsToCook
-                mealCollection={supperList}
-                iconName={salad_icon}
-                altProp={'salad icon'}
-                path={'supper'}
-              />
-            }
-          ></Route>
-          <Route
-            path="/cook/supper/edit"
-            element={<EditSingleMealProduct nameOfMeal={'supper'} />}
-          />
-          <Route
-            path="ingredients"
-            element={
-              <Ingredients
-                nameOfMealCollection={'supper'}
+              <MealHeader
+                title={'supper'}
+                backToCook={'/*'}
+                nameOfMeal={'supper'}
                 mealCollection={supperList}
               />
             }
-          ></Route>
-          <Route path="recipeList" element={<RecipeList />}></Route>
-          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-          <Route path="createRecipe" element={<CreateRecipe />} />
-        </Route>
-
-        {/* ***************** Snacks ***************** */}
-        <Route
-          path="/cook/snack"
-          element={
-            <MealHeader
-              title={'snacks'}
-              backToCook={'/*'}
-              nameOfMeal={'snack'}
-              mealCollection={snackList}
+          >
+            <Route
+              path="/cook/supper"
+              element={
+                <UsedProductsToCook
+                  mealCollection={supperList}
+                  iconName={salad_icon}
+                  altProp={'salad icon'}
+                  path={'supper'}
+                />
+              }
+            ></Route>
+            <Route
+              path="/cook/supper/edit"
+              element={<EditSingleMealProduct nameOfMeal={'supper'} />}
             />
-          }
-        >
+            <Route
+              path="ingredients"
+              element={
+                <Ingredients
+                  nameOfMealCollection={'supper'}
+                  mealCollection={supperList}
+                />
+              }
+            ></Route>
+            <Route path="recipeList" element={<RecipeList />}></Route>
+            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+            <Route path="createRecipe" element={<CreateRecipe />} />
+          </Route>
+
+          {/* ***************** Snacks ***************** */}
           <Route
             path="/cook/snack"
             element={
-              <UsedProductsToCook
-                mealCollection={snackList}
-                iconName={snack_icon}
-                altProp={'snack icon'}
-                path={'snack'}
-              />
-            }
-          ></Route>
-          <Route
-            path="/cook/snack/edit"
-            element={<EditSingleMealProduct nameOfMeal={'snack'} />}
-          />
-          <Route
-            path="ingredients"
-            element={
-              <Ingredients
-                nameOfMealCollection={'snack'}
+              <MealHeader
+                title={'snacks'}
+                backToCook={'/*'}
+                nameOfMeal={'snack'}
                 mealCollection={snackList}
               />
             }
-          ></Route>
-          <Route path="recipeList" element={<RecipeList />}></Route>
-          <Route path="favoriteFood" element={<FavoriteFood />}></Route>
-          <Route path="createRecipe" element={<CreateRecipe />} />
-        </Route>
-        {/* *********************************************************** */}
-        {/* <Route
+          >
+            <Route
+              path="/cook/snack"
+              element={
+                <UsedProductsToCook
+                  mealCollection={snackList}
+                  iconName={snack_icon}
+                  altProp={'snack icon'}
+                  path={'snack'}
+                />
+              }
+            ></Route>
+            <Route
+              path="/cook/snack/edit"
+              element={<EditSingleMealProduct nameOfMeal={'snack'} />}
+            />
+            <Route
+              path="ingredients"
+              element={
+                <Ingredients
+                  nameOfMealCollection={'snack'}
+                  mealCollection={snackList}
+                />
+              }
+            ></Route>
+            <Route path="recipeList" element={<RecipeList />}></Route>
+            <Route path="favoriteFood" element={<FavoriteFood />}></Route>
+            <Route path="createRecipe" element={<CreateRecipe />} />
+          </Route>
+          {/* *********************************************************** */}
+          {/* <Route
             path="/createNewProduct"
             element={<CreateProduct setToggleModal={setToggleModal} />}
           /> */}
-          <Route path='/'>
-        <Route path="/stock" element={<Stock />} />
-        </Route>
-        <Route path="/shoppingList" element={<ShoppingList />} />
-        {/* <Route path="*" element={< NoMatch/>} /> */}
-      </Routes>
-      <BottomBar />
+          <Route path="/">
+            <Route path="/stock" element={<Stock />} />
+          </Route>
+          <Route path="/shoppingList" element={<ShoppingList />} />
+          {/* <Route path="*" element={< NoMatch/>} /> */}
+        </Routes>
+        <BottomBar />
+      </ScrollToTop>
     </div>
   );
 }
