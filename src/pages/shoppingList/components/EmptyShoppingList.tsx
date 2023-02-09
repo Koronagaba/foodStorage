7efe3cf8826a6aba0a3a0d4ea56d9495c3db4f96
Koronagaba <killleon007@gmail.com>
2023-02-09 11:43:34 +0100

    New layout in 'Empty shopping list'

--- src/pages/shoppingList/components/EmptyShoppingList.tsx
+++ src/pages/shoppingList/components/EmptyShoppingList.tsx
@@ -1,14 +1,14 @@
 import { useTranslation } from 'react-i18next';
 
 import './EmptyShoppingList.css';
-import empty_list from '../../../icons/empty_list.png';
+import { Link } from 'react-router-dom';
 
 const EmptyShoppingList = () => {
   const { t } = useTranslation();
   return (
     <div className="empty-shopping-list">
       <p>{t('empty_shopping_list')}</p>
-      <img src={empty_list} alt={t('empty_shopping_list_alt')} />
+      <Link to="/stock">{t("add_products_to_shopping_list")}</Link>
     </div>
   );
 };
